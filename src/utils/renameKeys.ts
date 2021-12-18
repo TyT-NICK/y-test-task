1c3f9db073504c23ee5686dd5db4439e56665fd9
Artem Kuroptev <artem99k14@gmail.com>
2021-12-18 20:14:59 +0300

    added snake_case renamer

---
+++ src/utils/renameKeys.ts
@@ -0,0 +1,10 @@
+export const renameKeys = <T>(obj: object): T => {
+  return Object.entries(obj).reduce((acc, [key, val]) => {
+    const modifiedKey = key.replace(/_+([a-z])/g, (g) => g[g.length - 1].toUpperCase())
+
+    return { ...acc, [modifiedKey]: Array.isArray(val) ? renameKeysInArray(val) : val }
+  }, {}) as T
+}
+
+export const renameKeysInArray = <T extends Array<object>>(array: object[]): Array<T> =>
+  array.map((elem) => renameKeys(elem))
