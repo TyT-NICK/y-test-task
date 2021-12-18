e9b7668de7c58484f0f284b078f195c58c774a2b
Artem Kuroptev <artem99k14@gmail.com>
2021-12-18 20:49:35 +0300

    added useLogin hook

---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,21 @@
+import { useCallback } from 'react'
+
+import { auth } from 'src/utils'
+import { useLoginRequest } from './useRequest'
+
+const useLogin = () => {
+  const [requestLogin, pending] = useLoginRequest()
+
+  const login = useCallback(
+    (token?: string) => {
+      if (token) return auth.setToken(token)
+
+      requestLogin({ uuid: 'hello' }).then((response) => auth.setToken(response.access_token))
+    },
+    [requestLogin]
+  )
+
+  return [login, pending]
+}
+
+export default useLogin
